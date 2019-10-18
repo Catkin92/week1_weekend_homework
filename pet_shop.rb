88986f6403537e103b75ef80dbddebf6da0cc4ed
Catrin <catrinhughes92@gmail.com>
2019-10-18 19:47:33 +0100

    remove customer cash

--- pet_shop.rb
+++ pet_shop.rb
@@ -52,6 +52,10 @@ def add_pet_to_stock(shop, new_pet)
   shop[:pets] << new_pet
 end
 
-def customer_cash(customers)
-  return customers[:cash]
+def customer_cash(customer)
+  return customer[:cash]
+end
+
+def remove_customer_cash(customer, cash)
+  customer[:cash] -= cash
 end
