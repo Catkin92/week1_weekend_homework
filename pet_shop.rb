474c905d36fa35feaa7e996383a6bf8c2d1ad1bc
Catrin <catrinhughes92@gmail.com>
2019-10-18 18:33:50 +0100

    eighth and ninth test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,30 @@ end
 def stock_count(shop)
   return shop[:pets].count
 end
+
+def pets_by_breed(shop, breed)
+  pets_array = []
+  shop[:pets].each do |type|
+    if type[:breed] == breed
+    pets_array << type[:name]
+    end
+  end
+  p pets_array
+end
+# def pets_by_breed(shop, breed)
+#   pet_group = []
+#   for pet in shop
+#     if shop[1][:breed] == breed
+#       pet_group << shop[1][:name]
+#     end
+#   end
+#   return pet_group
+# end
+# #
+# pets_by_breed(@pet_shop, "British Shorthair")
+#
+# def find_pet_by_name(shop, name)
+#   if shop[:name] == name
+#     return name
+#   end
+# end
